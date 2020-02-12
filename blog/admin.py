bb495558ceff8399cca579ac1e65938245f23149
syr <fladb78@naver.com>
2020-02-12 17:34:56 +0900

    Create a comment model and manage comments.

--- blog/admin.py
+++ blog/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment
 
 admin.site.register(Post)   #관리자 페이지에서 만든모델(Post)보기
+admin.site.register(Comment)
--- blog/models.py
+++ blog/models.py
@@ -20,3 +20,20 @@ class Post(models.Model):   #모델을 정의하는 코드 장고모델임을 �
 
     def __str__(self):
         return self.title
+
+    def approved_comments(self):
+        return self.comments.filter(approved_comment=True)
+
+class Comment(models.Model):
+    post = models.ForeignKey('blog.Post', on_delete=models.CASCADE, related_name='comments')
+    author = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+    approved_comment = models.BooleanField(default=False)
+
+    def approve(self):
+        self.approved_comment = True
+        self.save()
+
+    def __str__(self):
+        return self.text
