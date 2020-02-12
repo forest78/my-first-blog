042d77a07fb2baaffb119c4da02cfaef1dad4941
syr <fladb78@naver.com>
2020-02-12 10:37:30 +0900

    Added views to create/edit blog post inside the site.

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,6 @@ from . import views #장고 함수 paht와 blog앱에서 사용할 모든 views�
 urlpatterns = [
     path('', views.post_list, name='post_list'),    #post_list라는 view가 루트 URL에 할당
     path('post/<int:pk>/', views.post_detail, name='post_detail'),  #post/<int:pk/>/는 URL 패턴 > url이 post문자를 포함, 정수값이 pk라는 변수로 뷰에 전송
+    path('post/new/', views.post_new, name='post_new'),
+    path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -2,13 +2,40 @@ from django.shortcuts import render
 from .models import Post
 from django.utils import timezone
 from django.shortcuts import render, get_object_or_404
-
+from .forms import PostForm
+from django.shortcuts import redirect
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html',{'posts': posts})    # request:사용자가 요청하는 모든것, 요청을 넘겨받아 render메서드를 호출합니다. 이 함수는 render메서드를 호출하여 받은 blog/post_list.html템플릿을 보여줌
 
 def post_detail(request, pk):
-    post = get_object_or_404(Post, pk=pk)
+    post = get_object_or_404(Post, pk=pk)   # pk에 해당하는 post가 없을 경우에는 404에러를 보여주고, 있을 경우에는 그 pk에 해당하는 Post를 post변수에 저장
     return render(request, 'blog/post_detail.html', {'post': post})
-    
+
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
+
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'blog/post_edit.html', {'form': form})
